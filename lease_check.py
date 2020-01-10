9fa2649cb6ee11e2909ff73acd02a7a7fcc4bdc5
Eric Blincow <eblincow@barracuda.com>
2020-01-09 21:21:59 -0800

    starting

---
+++ lease_check.py
@@ -0,0 +1,13 @@
+# lease check
+import datetime
+import pytz
+
+TOTAL = 10000.0 #miles
+PERDAY = 10000.0/365.0 #miles/day
+STARTED = 185.0 #miles
+STARTDATE =  datetime.datetime(2019, 11, 9, 0, 0, tzinfo=pytz.UTC)
+NOW = datetime.datetime.now(pytz.utc)
+DIFF = NOW - STARTDATE # how long have had car
+DAYS = DIFF.days
+
+
